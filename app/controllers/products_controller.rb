2cc877f083d38e3fb2ac5f1606ee28ad55b4e2ae
CalebFrederick <caleb@heckfamily.com>
2022-06-30 14:56:02 -0400

    Products added to database, Controller and Routes updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+
+    def show_all_method
+        products = Product.all 
+        render json: products.as_json
+    end
+
+    def first_product
+        product = Product.first
+        render json: product.as_json
+    end
+
 end
