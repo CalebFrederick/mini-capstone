75afc8e7fb9a151c58e10f49fc6e9259dd3700c4
CalebFrederick <caleb@heckfamily.com>
2022-06-30 12:54:43 -0400

    adds C9 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.new(name: "Cloud 9 - Jersey" price: 80 image_url: "https://cdn.shopify.com/s/files/1/0501/5685/products/2022-Gameday-Full-Sponsor-Jersey-DarkBlue_Blockchain_b54d3088-91b2-451d-aaf7-ffb7da49d37e_1400x.png?v=1646230582" description: "Exuding a deep sense of calm, the PUMA x CLOUD9 pro kit features a tranquil, ocean-worthy color palette on lightweight and sustainable fabrics. Blues and whites reference the crashing waves and brings a true Santa Monica vibe to this years lineup")
+
+Product.new(name: "Cloud 9 - Camper Hat" price: 25 image_url: "https://cdn.shopify.com/s/files/1/0501/5685/products/6206bb85-e881-4cd7-b564-45632f44d304_1400x.png?v=1626154399" description: "Always on. Rep PUMA and Cloud9 in this classic cap, featuring a sleek five panel shape, an adjustable closure and classic branding.")
+
+Product.new(name: "Cloud 9 - Joggers" price: 55 image_url: "https://cdn.shopify.com/s/files/1/0501/5685/products/Black-Friday-Jogger_1000x.png?v=1647988015" description: "Casual and competitive comes together with the PUMA x Cloud9 Sweat Pant. Comfort and style are taken to the next level with sleek colors, cuffed ankles, a drawcord waistband, and back yoke. Put them on and show out with the sticks.")
